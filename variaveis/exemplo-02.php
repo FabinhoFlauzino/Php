0ec26f8e569da3b5f47f8b125e473a0ab63f8381
Fabinho Flauzino <fabioflauzino@hotmail.com>
2019-02-03 18:21:37 -0200

    exemplo-02

---
+++ variaveis/exemplo-02.php
@@ -0,0 +1,26 @@
+<?php
+    $anoNascimento = 1985;
+
+    $nome = "Fabinho";
+
+    $sobrenome = "Flauzino";
+
+    $nomeCompleto = $nome." ".$sobrenome;
+    //concatenação entre duas variaveis usasse o "."
+    //não esquecer do espaço para não ficar colado as palavras
+
+    echo $nome;
+
+    echo "<br/>"
+
+    if(isset($nome)){
+        //isset verifica se a variavel $nome ja esta sendo usada e mostra na tela
+        echo $nome;
+
+    }
+
+    unset($nome);
+    //unset apaga uma variavel que ja esta sendo usada da memória mas nao 
+    //perde as declarações anteriores até o uso do unset
+
+?>
