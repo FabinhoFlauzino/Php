502f8d2cd15f7170ece1d1a590977941bb0460ca
FabinhoFlauzino <fabioflauzino@hotmail.com>
2019-07-06 22:30:20 -0300

    Trabalhando com DAO no PHP

---
+++ PDO/exemplo02.php
@@ -0,0 +1,25 @@
+<?php 
+
+    $conn = new PDO("mysql:dbname=dbphp7;host=localhost", "root","");
+
+    $stmt = $conn->prepare("INSERT  INTO tb_usuarios (deslogin,dessenha) VALUES( :LOGIN, :PASSWORD)");
+
+    $login = "jose";
+    $password = "1234";
+
+    $stmt->bindParam(":LOGIN", $login);
+    $stmt->bindParam(":PASSWORD", $password);
+
+    $stmt->execute();
+
+    $login = "Fulano";
+    $password = "4567";
+
+    $stmt->bindParam(":LOGIN", $login);
+    $stmt->bindParam(":PASSWORD", $password);
+
+    $stmt->execute();
+    
+    echo "Inserido com sucesso";
+
+?>
